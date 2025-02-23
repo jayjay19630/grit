fa030b21f5291b96ca14f16dfd3c07cf9463b498
Jo An <positivelyjon@gmail.com>
2025-02-24 00:11:25 +0800

    feat: create cat-file command

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import zlib
 
 
 def main():
@@ -15,7 +16,21 @@ def main():
         os.mkdir(".git/refs")
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
-        print("Initialized git directory")
+        print("Initialized git directory.")
+    elif command == "cat-file":
+        param_index = sys.argv.index("-p")
+        if not param_index:
+            raise RuntimeError("Please use the -p marker to denote object hash!")
+
+        object_hash = sys.argv[param_index + 1]
+        object_dirname = object_hash[:2]
+        object_filename = object_hash[2:]
+        with open(f".git/objects/{object_dirname}/{object_filename}", "rb") as f:
+            compressed_bytes = f.read()
+            decompressed_bytes = zlib.decompress(compressed_bytes)
+            content = decompressed_bytes.split(b"\x00")[1].decode("utf-8")
+        print(content)
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
